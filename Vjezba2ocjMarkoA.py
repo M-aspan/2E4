b0043fab402be2bbc6d594b0a9205abf32ec4f0b
Marko <marko.aspan.2009@gmail.com>
2025-11-10 14:19:30 +0100

    Add files via upload

---
+++ Vjezba2ocjMarkoA.py
@@ -0,0 +1,35 @@
+print ("U1=8 volti")
+print ("U2=5 volti")
+print ("Izračunajmo ukupni napon U12")
+U1 = 8
+U2 = 5
+def zbroj_U1_i_U2 ()
+  print ("U1+U2")
+  print ("13")
+  my function()
+broj = 0
+
+U1 = 8
+while U1 < 15:
+  print(U1)
+  U1 = 8
+
+try:
+  print(13)
+except:
+  print("Gadna pogreška")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
