b72bf880f23f1a317c71e2a21cda47ee9303658c
Marko <marko.aspan.2009@gmail.com>
2025-11-24 14:10:28 +0100

    Add files via upload

---
+++ python-ispravak.py
@@ -0,0 +1,53 @@
+#radim kalkulator za kalkulator za preračuravanje digitalnih veličina
+print ("Kalkulator za digitalne veličine")
+x = 1024
+x2 = 2048
+y =  4096.00 #ovo je float
+print (x)
+print (x2)
+print (y)
+print ("Ovo su faktori")
+print ("Upiši x+x2")
+
+print("prvi faktor:")
+prvi = input()
+
+print("drugi faktor:")
+prvi = input()
+
+if x+x2 < 3072:
+  print ("krivo")
+
+elif x+x2 == 3072:
+  print ("točno")
+  print ("rješenje je 3072")
+
+print ("upiši x*y")
+
+print("prvi faktor:")
+prvi = input()
+
+print("drugi faktor:")
+prvi = input()
+
+if x+y == 4194304.00:
+ print ("točno")
+ print ("rješenje je 4194394.00")
+
+elif x*y < 4194304.00:
+  print ("krivo")
+
+
+while x+y == 3072:
+ print (x+y)
+
+else:
+ print ("krivo")
+
+
+#sve rade ali iz nekog razloga trebam kliknut run 3 puta da se pojave svi dijelovi
+
+
+
+ 
+ 
